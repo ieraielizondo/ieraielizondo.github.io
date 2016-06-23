dfa96329df647b980886813bb6bd26dda8f5d4e7
Ierai Elizondo <ieraielizondo@gmail.com>
2016-06-23 11:14:53 +0200

    new design, typed.js added

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+	$(".subtitle").typed({
+		strings: ["Desarrollador  de software.", "Desarrollador web."],// ^1000 para parar xx milisegundos
+		typeSpeed: 30, // typing speed
+		backDelay: 500, // pause before backspacing
+		loop: true, // loop on or off (true or false)
+		loopCount: false, // number of loops, false = infinite
+	});
+});
